0f3714b8feceb40a4df0a43e44729a143297cc01
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-08-21 09:48:52 -0700

    lab completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import Links from "./Links";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
@@ -11,8 +12,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color={user.color} name={user.name} city={user.city} />
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import App from "./App";
 
 function Home(props) {
   return (
